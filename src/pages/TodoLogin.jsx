ca0a61fe31e2b1a8140624749399b90fd81cd9a3
maaz80 <maazforlap@gmail.com>
2025-05-03 23:20:40 +0530

    Fixing the auto otp fill

--- src/pages/TodoLogin.jsx
+++ src/pages/TodoLogin.jsx
@@ -185,12 +185,12 @@ function TodoLogin() {
         attemptOtpAutofill();
       }, 4000);
     }
-  }, [otpSent]); 
+  }, [otpSent]);
 
   // OTP verify
   const verifyOtp = async (e) => {
     e.preventDefault();
-  
+
     if (!otp || otp.length !== 6) {
       toast.error("Please enter a valid 6-digit OTP!");
       return;
@@ -468,22 +468,21 @@ function TodoLogin() {
           <form className="space-y-4" onSubmit={verifyOtp} id="otp-form">
             {/* OTP input */}
             <OTPInput
-  value={otp}
-  onChange={(value) => setValue('otp', value)}
-  numInputs={6}
-  renderSeparator={<span className="mx-1"></span>}
-  renderInput={(props) => (
-    <input
-      {...props}
-      className={`w-10 h-12 text-center border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-600 outline-none ${
-        theme === "dark" ? "bg-gray-700 text-gray-300" : "bg-white text-gray-700"
-      }`}
-      autoComplete="one-time-code"
-      inputMode="numeric"
-    />
-  )}
-  containerStyle="flex justify-center gap-2"
-/>
+              value={otp}
+              onChange={(value) => setValue('otp', value)}
+              numInputs={6}
+              renderSeparator={<span className="mx-1"></span>}
+              renderInput={(props) => (
+                <input
+                  {...props}
+                  className={`w-24 h-12 text-center border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-600 outline-none ${theme === "dark" ? "bg-gray-700 text-gray-300" : "bg-white text-gray-700"
+                    }`}
+                  autoComplete="one-time-code"
+                  inputMode="numeric"
+                />
+              )}
+              containerStyle="flex justify-center gap-2"
+            />
             {errors.otp && (
               <p className="text-red-500 text-sm -mt-3">{errors.otp.message}</p>
             )}
